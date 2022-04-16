114eb3f198a194c262c9fc4d676ba2e63ffa3ca5
Omar Hesham <oommaarr.220022@gmail.com>
2022-04-16 06:00:59 +0200

    Admin data manipulation process (select, insert, delete, and update)

--- Models/Admin/Admin.cpp
+++ Models/Admin/Admin.cpp
@@ -1,1 +1,62 @@
 #include "Admin.h"
+
+
+FileHandler Admin::fileHandler(
+	"admin",
+	{ "NationalID", "Password"},
+	"NationalID"
+);
+
+
+Admin::Admin() {
+	nationalID = password = "";
+};
+
+
+Admin::Admin(string nationalID, string password) : nationalID(nationalID), password(password) {}
+
+
+bool Admin::insert() {
+	return Admin::fileHandler.insert(this->toMap());
+}
+
+
+void Admin::update() {
+	Admin::fileHandler.update(
+		this->toMap(),
+		{ {Admin::fileHandler.primaryKey, "=", this->nationalID} }
+	);
+}
+
+
+void Admin::remove() {
+	Admin::fileHandler.remove({ {Admin::fileHandler.primaryKey, "=", this->nationalID} });
+}
+
+
+vector<Admin> Admin::select(vector<condition> conditions) {
+	vector<map<string, string>> records = Admin::fileHandler.select(conditions);
+	vector<Admin> admins;
+
+	for (int i = 0; i < records.size(); i++) {
+		admins.push_back(Admin::fromMap(records[i]));
+	}
+
+	return admins;
+}
+
+
+map<string, string> Admin::toMap() {
+	return {
+		{Admin::fileHandler.headers[0], this->nationalID},
+		{Admin::fileHandler.headers[1], this->password}
+	};
+}
+
+
+Admin Admin::fromMap(map<string, string> data) {
+	return Admin(
+		data[Admin::fileHandler.headers[0]],
+		data[Admin::fileHandler.headers[1]]
+	);
+}
--- Models/Admin/Admin.h
+++ Models/Admin/Admin.h
@@ -1,9 +1,59 @@
 #pragma once
-#include "../../Core/Model/Model.h"
+#include "../../Core/FileHandler/FileHandler.h"
 
 
-class Admin :
-    private Model
+class Admin 
 {
+	static FileHandler fileHandler;
+
+public:
+	string nationalID;
+	string password;
+
+	Admin();
+
+	Admin(string NationalID, string password);
+
+
+	/**
+	 * @brief Insert the Admin into the database
+=	 * @return bool shows if the insertation process done successfully or not (true -> inserted successfully, false -> redundant National ID)
+	 */
+	bool insert();
+
+
+	/**
+	 * @brief Update the Admin in the database
+	 */
+	void update();
+
+
+	/**
+	 * @brief Remove the Admin from the database
+	 */
+	void remove();
+
+
+	/**
+	 * @brief Retrive specific admins from the database according to some conditions
+	 * @param vector<condition> conditions to be applied into the records to filter them
+	 * @return vector<Admin> contains the retrived admins
+	 */
+	static vector<Admin> select(vector<condition> conditions = {});
+
+private:
+	/**
+	 * @brief Convert the admin object to map
+	 * @return map<string, string> of the admin attributes
+	 */
+	map<string, string> toMap();
+
+
+	/**
+	 * @brief Convert the admin maped data to an admin object
+	 * @param map<string, string> data of the admin
+	 * @return Admin object obtained from the map
+	 */
+	static Admin fromMap(map<string, string> data);
 };
 
