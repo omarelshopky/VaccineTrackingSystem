e374ae6678969510e4d7eb8b3515f315c040ee2d
Omar Hesham <oommaarr.220022@gmail.com>
2022-04-16 05:50:17 +0200

    User data manipulation process (select, insert, delete, and update)

--- Models/User/User.cpp
+++ Models/User/User.cpp
@@ -1,1 +1,79 @@
 #include "User.h"
+
+FileHandler User::fileHandler(
+	"user",
+	{"FullName", "NationalID", "Password", "Gender", "Age", "Country", "Government", "isVaccinated", "DosesNumber"},
+	"NationalID"
+);
+
+
+User::User() {
+	fullName = nationalID = password = gender = country = government = "";
+	age = isVaccinated = dosesNumber = 0;
+};
+
+
+User::User(string fullName, string nationalID, string password, string gender, 
+	int age, string country, string government, bool isVaccinated, int dosesNumber) :
+	fullName(fullName), nationalID(nationalID), password(password), gender(gender), 
+	age(age), country(country), government(government), isVaccinated(isVaccinated), dosesNumber(dosesNumber) {}
+
+
+bool User::insert() {
+	return User::fileHandler.insert(this->toMap());
+}
+
+
+void User::update() {
+	User::fileHandler.update(
+		this->toMap(),
+		{ {User::fileHandler.primaryKey, "=", this->nationalID} }
+	);
+}
+
+
+void User::remove() {
+	User::fileHandler.remove({ {User::fileHandler.primaryKey, "=", this->nationalID} });
+}
+
+
+vector<User> User::select(vector<condition> conditions) {
+	vector<map<string, string>> records = User::fileHandler.select(conditions);
+	vector<User> users;
+
+	for (int i = 0; i < records.size(); i++) {
+		users.push_back(User::fromMap(records[i]));
+	}
+
+	return users;
+}
+
+
+map<string, string> User::toMap() {
+	return {
+		{User::fileHandler.headers[0], this->fullName},
+		{User::fileHandler.headers[1], this->nationalID},
+		{User::fileHandler.headers[2], this->password},
+		{User::fileHandler.headers[3], this->gender},
+		{User::fileHandler.headers[4], to_string(this->age)},
+		{User::fileHandler.headers[5], this->country},
+		{User::fileHandler.headers[6], this->government},
+		{User::fileHandler.headers[7], to_string(this->isVaccinated)},
+		{User::fileHandler.headers[8], to_string(this->dosesNumber)}
+	};
+}
+
+
+User User::fromMap(map<string, string> data) {
+	return User(
+		data[User::fileHandler.headers[0]],
+		data[User::fileHandler.headers[1]],
+		data[User::fileHandler.headers[2]],
+		data[User::fileHandler.headers[3]],
+		stoi(data[User::fileHandler.headers[4]]),
+		data[User::fileHandler.headers[5]],
+		data[User::fileHandler.headers[6]],
+		stoi(data[User::fileHandler.headers[7]]),
+		stoi(data[User::fileHandler.headers[8]])
+	);
+}
--- Models/User/User.h
+++ Models/User/User.h
@@ -1,8 +1,66 @@
 #pragma once
-#include "../../Core/Model/Model.h"
+#include "../../Core/FileHandler/FileHandler.h"
 
-class User :
-    private Model
+class User 
 {
+	static FileHandler fileHandler;
+
+public:
+	string fullName;
+	string nationalID;
+	string password;
+	string gender;
+	int age;
+	string country;
+	string government;
+	bool isVaccinated;
+	int dosesNumber;
+
+
+	User();
+
+	User(string fullName, string NationalID, string password, string gender, int age, string country, string government, bool isVaccinated, int dosesNumber);
+
+
+	/**
+	 * @brief Insert the User into the database
+=	 * @return bool shows if the insertation process done successfully or not (true -> inserted successfully, false -> redundant National ID)
+	 */
+	bool insert();
+
+
+	/**
+	 * @brief Update the User in the database
+	 */
+	void update();
+
+
+	/**
+	 * @brief Remove the User from the database
+	 */
+	void remove();
+
+
+	/**
+	 * @brief Retrive specific users from the database according to some conditions
+	 * @param vector<condition> conditions to be applied into the records to filter them
+	 * @return vector<User> contains the retrived users
+	 */
+	static vector<User> select(vector<condition> conditions = {});
+
+private:
+	/**
+	 * @brief Convert the user object to map
+	 * @return map<string, string> of the user attributes
+	 */
+	map<string, string> toMap();
+
+
+	/**
+	 * @brief Convert the user maped data to a user object
+	 * @param map<string, string> data of the user
+	 * @return User object obtained from the map
+	 */
+	static User fromMap(map<string, string> data);
 };
 
