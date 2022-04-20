bef82c46073b482b76462e73255e3b975e6c2599
Enas Ahmed <enasahmed807@gmail.com>
2022-04-20 07:21:30 +0200

    the logic finished

--- Controllers/AdminController/AdminController.h
+++ Controllers/AdminController/AdminController.h
@@ -24,12 +24,49 @@ public:
 	/***	Enas	***/ // LoginView
 	vector<float> getStatistics();
 
+    /**
+	* @brief gets the statistic about doses
+	* @return map<string, float> -> the dose and its precentage 
+	*/
+	map<int, float> statOfAllDoses();
+
+	/**
+	* @brief gets the statistic about genders
+	* @return map<string, float> -> the male and the precentage of its dose
+	*/
+	map<string, float> statOfAllMales();
+
+	/**
+	* @brief to get the statistic about governments 
+	* @return map<string, float> -> the govenment and the precentage of dose taken in
+	*/
+	map<string, float> statOfAllGovernment();
+
 private:
-	float statOfFirstDose();
-	float statOfBothDose(); 
-	float statOfNoDose();
+	/**
+	* @brief gets the precntage of the spacific number of doses
+	* @param int to select the number of doses
+	* @return number -> the precentage of doses the user took 
+	*/
+	float statOfDoses(int numOfDoses);
+
+	/**
+	* @brief gets the precentage of the spacific number of males
+	* @return number -> the precentage of males
+	*/
 	float statOfMale();
+
+	/**
+	* @brief gets the precentage of the spacific number of governments
+	* @param string to select the government taken in
+	* @return number -> the precentage of government
+	*/
 	float statOfGovernment(string government);
 
+	/**
+	* @brief gets the number of users in database
+    * @return integer -> the precentage number of users
+	*/
+	int  numberOfUsers();
 };
 
