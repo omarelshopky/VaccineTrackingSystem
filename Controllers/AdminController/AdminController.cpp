b4f5b6d6f7c66047385781ba168fa04996842bdd
Abd El Salam <mernaabdallah89@gmail.com>
2022-05-03 02:57:58 +0200

    admin logic is done

--- Controllers/AdminController/AdminController.cpp
+++ Controllers/AdminController/AdminController.cpp
@@ -1,6 +1,120 @@
 #include "AdminController.h"
+using namespace std;
 
 
 AdminController::AdminController() {
 
 }
+
+vector<User> AdminController::viewUsers() {
+
+	vector<User> users = User ::select();
+	
+	for (int i = 0; i < users.size(); i++)
+	{
+		cout << "                    user number " << i + 1 << endl;
+		cout <<  "  FullName : " << users[i].fullName << endl;
+		cout  << "  NationalID : " << users[i].nationalID << endl;
+		cout  << "  Password : " << users[i].password << endl;
+		cout <<  "  Gender : " << users[i].gender << endl;
+		cout <<  "  Age : " << users[i].age << endl;
+		cout <<  "  Country : " << users[i].country << endl;
+		cout <<  "  Government : " << users[i].government << endl;
+		cout <<  "  isVaccinated : " << users[i].isVaccinated << endl;
+		cout <<  "  DosesNumber : " << users[i].dosesNumber << endl;
+		cout << "*******************************************************************************" << endl;
+	}
+	return users;
+}
+
+
+
+
+User AdminController::viewUser(string nationalID) {
+	User user;
+	bool found = false;
+	vector<User> users = User::select();
+	for (int i = 0; i < users.size(); i++)
+	{
+		if (users[i].nationalID == nationalID)
+		{
+			found = true;
+			cout << "  the data of user which has nationalID -> " << users[i].nationalID << " is : " << endl;
+			cout << "  FullName : " << users[i].fullName << endl;
+			cout << "  NationalID : " << users[i].nationalID << endl;
+			cout << "  Password : " << users[i].password << endl;
+			cout << "  Gender : " << users[i].gender << endl;
+			cout << "  Age : " << users[i].age << endl;
+			cout << "  Country : " << users[i].country << endl;
+			cout << "  Government : " << users[i].government << endl;
+			cout << "  isVaccinated : " << users[i].isVaccinated << endl;
+			cout << "  DosesNumber : " << users[i].dosesNumber << endl;
+			cout << "*******************************************************************************" << endl;
+			return users[i];
+		}
+	}
+	
+	if (found == false) {
+		cout << " user not found , please enter a valid nationalID and try again :) " << endl;
+		return user;
+		}
+		
+}
+
+
+
+
+void AdminController::deleteUsers() {
+	vector<User> users = User::select();
+	for (int i = 0; i < users.size(); i++) {
+		users[i].remove();
+		cout << "the data has been removed for user number : " << i+1 << endl;
+	}
+
+}
+
+
+
+
+void AdminController::deleteUser(string nationalID) {
+	bool found = false;
+	vector<User> users = User::select();
+	for (int i = 0; i < users.size(); i++)
+	{
+		if (users[i].nationalID == nationalID)
+		{
+			found = true;
+		    users[i].remove();
+		    cout << users[i].fullName << " " << "is removed " << endl;
+		}
+	}
+
+	if (found == false) {
+		cout << " user not found , please enter a valid nationalID and try again :) " << endl;
+	}
+
+}
+
+
+
+
+void AdminController::giveAdminPrivilage(string nationalID , string password) {
+	bool found = false;
+	vector<User> users = User::select();
+	for (int i = 0; i < users.size(); i++)
+	{
+		if (users[i].nationalID == nationalID)
+		{
+			found = true;
+			Admin admin(users[i].nationalID, password);
+			admin.insert();
+			cout << "the user " << users[i].fullName << " is now an admin " << endl;
+		}
+	}
+
+	if (found == false) {
+		cout << " user  not found , try again " << endl;
+	}
+} 
+
+
--- Controllers/AdminController/AdminController.h
+++ Controllers/AdminController/AdminController.h
@@ -13,12 +13,44 @@ public:
 	void login(string nationalID, string password);
 
 
-	/***	Merna	***/ // AdminPrivilageView
-	void giveAdminPrivilage(string nationalID);
+	/***	Merna	***/ 
+
+
+	/**
+	 * @brief Give Admin Privilege to a normal user
+	 * @param string nationalID  of the user , string password to make him an admin
+   */
+
+	void giveAdminPrivilage(string nationalID , string password);
+
+
+	/**
+     * @brief View all the users that stored in the database
+     * @return vector<User> contains the all stored users in the database
+   */
 	vector<User> viewUsers();
+
+
+	/**
+     * @brief Retrive specific user from the database according to his nationalID
+     * @param string nationalID of the user
+     * @return User which has the same nationalID
+    */
 	User viewUser(string nationalID);
+
+
+	/**
+     * @brief Delete all the users from the database
+    */
 	void deleteUsers();
+
+
+	/**
+	 * @brief Delete specific user from the database according to his nationalID
+	 * @param string nationalID of the user
+	*/
 	void deleteUser(string nationalID);
+
 
 
 	/***	Enas	***/ // LoginView
