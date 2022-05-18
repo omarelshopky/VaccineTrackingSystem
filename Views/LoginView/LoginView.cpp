c535df401fe67b3ade52341315ba2aab6a72429f
Omar Hesham <omar.elshopky202@gmail.com>
2022-05-18 20:02:45 +0200

    Login View UI Design

--- Views/LoginView/LoginView.cpp
+++ Views/LoginView/LoginView.cpp
@@ -1,12 +1,75 @@
 #include "LoginView.h"
+#include "ui_LoginView.h"
 
 
-LoginView::LoginView(QWidget *parent)
+
+
+LoginView::LoginView(QStackedWidget *widgetStack, QWidget* parent)
 	: QMainWindow(parent)
 {
+	this->widgetStack = widgetStack;
+
 	setupUi(this);
+
+	// Initialize components prop
+	nationalIDInput->setValidator(new QDoubleValidator(0, 100, 2, this)); //nationalId accept only numbers
+	this->clearView();
+
+	// Attach signals function
+    connect(loginBtn, &QPushButton::clicked, this, &LoginView::login);
+	connect(userTypeBtn, &QPushButton::clicked, this, &LoginView::changeUserType);
+	connect(registerBtn, &QPushButton::clicked, this, &LoginView::goToRegister);
 }
 
-LoginView::~LoginView()
-{
+
+LoginView::~LoginView() {}
+
+
+void LoginView::login() {
+	bool state = false;
+	if (isAdmin) {
+		 state = adminController.login(nationalIDInput->text().toStdString(), passInput->text().toStdString());
+	}
+	else {
+	     state = userController.login(nationalIDInput->text().toStdString(), passInput->text().toStdString());
+	}
+
+	if (state) {
+		this->clearView();
+		widgetStack->setCurrentIndex((isAdmin)? 3 : 2);
+	}
+	else {
+		errorLbl->show();
+	}
+}
+
+
+void LoginView::changeUserType() {
+	this->clearView();
+
+	if (isAdmin) {
+		userTypeBtn->setText("As User");
+		NoAccLbl->show();
+		registerBtn->show();
+	}
+	else {
+		userTypeBtn->setText("As Admin");
+		NoAccLbl->hide();
+		registerBtn->hide();
+	}
+	isAdmin = !isAdmin;
+}
+
+
+void LoginView::goToRegister() {
+	this->clearView();
+
+	widgetStack->setCurrentIndex(1);
+}
+
+
+void LoginView::clearView() {
+	nationalIDInput->clear();
+	passInput->clear();
+	errorLbl->hide();
 }
--- Views/LoginView/LoginView.h
+++ Views/LoginView/LoginView.h
@@ -1,13 +1,30 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QStackedWidget>
+#include <qvalidator.h>
 #include "ui_LoginView.h"
+#include "../../Controllers/AdminController/AdminController.h"
+#include "../../Controllers/UserController/UserController.h"
+
+
 
 class LoginView : public QMainWindow, public Ui::LoginView
 {
 	Q_OBJECT
 
+    AdminController adminController;
+	UserController userController;
+	bool isAdmin = false;
+	QStackedWidget *widgetStack;
+
 public:
-	LoginView(QWidget *parent = Q_NULLPTR);
+	LoginView(QStackedWidget *widgetStack, QWidget *parent = Q_NULLPTR);
 	~LoginView();
+
+private:
+	void login();
+	void changeUserType();
+	void goToRegister();
+	void clearView();
 };
